19cd7213c18935283bfccca85ab586e468d4edd1
pushery <hello@pushery.com>
2026-03-31 10:13:48 +0200

    Create WireKitServiceProvider.php

---
+++ src/WireKitServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pushery\WireKit;
+
+use Illuminate\Support\ServiceProvider;
+
+class WireKitServiceProvider extends ServiceProvider
+{
+    // Coming soon.
+}
